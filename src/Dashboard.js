e6e9cb472be550c93ac381716ee7dae0b5ea0b72
Emma Blott <blott23@gmail.com>
2021-02-12 09:23:04 +0000

    tidy up dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,48 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios' 
+
 import Amplify, { API , Auth} from 'aws-amplify';
 import {
   Link
 } from "react-router-dom";
-
-// export default function Dashboard() {
-//   const proofOfConceptApi = "https://bz2j9g1je4.execute-api.eu-west-1.amazonaws.com/dev/proof"
-//   const [content, setContent] = useState()
-//   const [loading, setLoading] = useState(true)
-
-//   // axios.defaults.headers.common['Content-Type'] ='application/json;charset=utf-8';
-//   // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
-//   useEffect(()=> {
-//     setLoading(true)
-//     let cancel
-//     axios.get(proofOfConceptApi, {
-//       cancelToken: new axios.CancelToken (c => cancel = c)
-//     }).then(res =>{
-//       setLoading(false)
-//       setContent(res.data)
-//     })
-//     return () => {cancel()}
-//   },[])
-
-
-  
-//   if (loading) return (
-//   <div>
-//     <p>"Loading..."</p><br/>
-//     <Link to="/">Home Page</Link><br/>
-//     <Link to="/auth">Log in</Link>
-//   </div>
-//   )
-
-//   return (
-//     <div>
-//       {content}<br/>
-//       <Link to="/">Home Page</Link><br/>
-//       <Link to="/auth">Log in</Link><br/>
-//     </div>
-//   )
-// }
 
 function Dashboard() {
   const [response, setResponse] = useState();
@@ -50,9 +11,6 @@ function Dashboard() {
   const apiName = 'proofofconcept';
   const path = '/proof'; 
   const myInit = { };
-  //   headers: { 
-  //     Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
-  //   }}
   
     useEffect(()=> {
           setLoading(true)
@@ -70,16 +28,7 @@ function Dashboard() {
           });
           return () => {cancel()}
         },[])
-  // API
-  //   .get(apiName, path, myInit)
-  //   .then(response => {
-  //     setResponse(response.data)
-  //   })
-  //   .catch(error => {
-  //     console.log("oh no, an error!")
-  //     console.log(error.response);
-  //     setResponse(error.response)
-  //  });
+ 
   if (loading) return (
       <div>
         <p>"Loading..."</p><br/>
